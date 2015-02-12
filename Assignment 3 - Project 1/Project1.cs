c057bc94c616c4785c12e40e39c1d209ce179cd7
Hods <hadielatabani@gmail.com>
2015-02-11 20:31:25 -0500

    Initial Base VS C# Project

---
+++ Assignment 3 - Project 1/Project1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3___Project_1
+{
+    class Project1
+    {
+        static void Main(string[] args)
+        {
+            string burger;
+            string pizza;
+            string exit;
+            int burgerb;
+            int pizzab;
+            int exitb;
+
+            Console.WriteLine("Welcome to Project 1");
+            Console.WriteLine(burger);
+            Console.WriteLine(pizza);
+        }
+    }
+}
