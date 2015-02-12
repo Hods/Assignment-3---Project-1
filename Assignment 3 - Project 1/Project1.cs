d6b80c6668726bc9326e89d13cf7ccf5a1bc7375
Hods <hadielatabani@gmail.com>
2015-02-11 21:04:54 -0500

    Added Menu

--- Assignment 3 - Project 1/Project1.cs
+++ Assignment 3 - Project 1/Project1.cs
@@ -10,16 +10,27 @@ namespace Assignment_3___Project_1
     {
         static void Main(string[] args)
         {
-            string burger;
-            string pizza;
-            string exit;
-            int burgerb;
-            int pizzab;
-            int exitb;
+            string burger = ("1. Would you like to eat a Burger? ");
+            string pizza = ("2. Would you like to eat Pizza? ");
+            string option;
+            int optionb;
 
             Console.WriteLine("Welcome to Project 1");
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("What would you like to eat? ");
             Console.WriteLine(burger);
             Console.WriteLine(pizza);
+            Console.WriteLine();
+
+            Console.Write("Choose an option (1-3): ");
+            option = Console.ReadLine();
+            optionb = Convert.ToInt32(option);
+
+
+            Console.WriteLine("--------------------------");
+            Console.Write("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
